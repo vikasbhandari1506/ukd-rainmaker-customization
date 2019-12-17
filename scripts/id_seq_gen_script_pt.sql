9ff930c9af66553b7325778e29f0e7af47694993
Hemanth <k.hemanth@egovernments.org>
2019-12-17 19:28:57 +0530

    Merge UAT to masters

---
+++ scripts/id_seq_gen_script_pt.sql
@@ -0,0 +1,15 @@
+DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_haldwani;
+DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_haridwar;
+DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_dehradun;
+DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_roorkee;
+DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_rudrapur;
+DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_kashipur;
+DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_rishikesh;
+
+CREATE SEQUENCE seq_eg_pt_ptid_uk_haldwani;
+CREATE SEQUENCE seq_eg_pt_ptid_uk_haridwar;
+CREATE SEQUENCE seq_eg_pt_ptid_uk_dehradun;
+CREATE SEQUENCE seq_eg_pt_ptid_uk_roorkee;
+CREATE SEQUENCE seq_eg_pt_ptid_uk_rudrapur;
+CREATE SEQUENCE seq_eg_pt_ptid_uk_kashipur;
+CREATE SEQUENCE seq_eg_pt_ptid_uk_rishikesh;
