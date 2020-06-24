67b950064169c00aa3634e15df4702bcfd07e548
Siva Nalluri <siva.nalluri@egovernments.org>
2020-06-24 13:24:34 +0530

    Configuring the sms language

--- ukd-custom-service/src/main/java/org/egov/ukdcustomservice/service/LocalizationService.java
+++ ukd-custom-service/src/main/java/org/egov/ukdcustomservice/service/LocalizationService.java
@@ -20,46 +20,44 @@ import lombok.extern.slf4j.Slf4j;
 public class LocalizationService {
 
     private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(LocalizationService.class);
-    
+
     @Autowired
-	private ObjectMapper mapper;
+    private ObjectMapper mapper;
 
     @Autowired
     private RestTemplate restTemplate;
-    
+
     @Value("${egov.localization.host}")
     private String localizartionHost;
 
     @Value("${egov.localization.search.endpoint}")
     private String endpoint;
 
-
+    @Value("${egov.localization.language}")
+    private String language;
 
     public String getResult(String key, String module, RequestInfo requestInfo) {
-        
+
         StringBuilder uri = new StringBuilder();
 
-        uri.append(localizartionHost)
-            .append(endpoint)
-            .append("?tenantId=uk")
-            .append("&module=")
-            .append(module)
-            .append("&locale=hi_IN");
+        uri.append(localizartionHost).append(endpoint).append("?tenantId=uk").append("&module=").append(module)
+                .append("&locale=").append(language);
         ArrayList<String> message = new ArrayList<String>();
-		Map response = null;
-		log.info("URI: "+uri.toString());
-		try {
-			log.info("Request: " + mapper.writeValueAsString(requestInfo));
+        Map response = null;
+        log.info("URI: " + uri.toString());
+        try {
+            log.info("Request: " + mapper.writeValueAsString(requestInfo));
             response = restTemplate.postForObject(uri.toString(), requestInfo, Map.class);
             String jsonString = new JSONObject(response).toString();
             log.info(jsonString);
-            message = (ArrayList<String>)JsonPath.parse(jsonString).read("$.messages[?(@.code=='"+key+"')].message");
-            
-        } catch(Exception e) {
-			log.error("Exception while fetching from searcher: ",e);
-		}
-		
-		return message.size() >= 1 ? message.get(0) : null;
-	}
+            message = (ArrayList<String>) JsonPath.parse(jsonString)
+                    .read("$.messages[?(@.code=='" + key + "')].message");
+
+        } catch (Exception e) {
+            log.error("Exception while fetching from searcher: ", e);
+        }
+
+        return message.size() >= 1 ? message.get(0) : null;
+    }
 
 }
