0aa13d5614886fc31f8c5a375ab60ad8c7e813dd
Siva Nalluri <siva.nalluri@egovernments.org>
2020-08-25 15:07:42 +0530

    cleaning the duplicate service

---
+++ ukd-custom-service/src/main/java/org/egov/ukdcustomservice/web/controller/LogController.java
@@ -0,0 +1,86 @@
+/*
+ * eGov suite of products aim to improve the internal efficiency,transparency,
+ *    accountability and the service delivery of the government  organizations.
+ *
+ *     Copyright (C) <2020>  eGovernments Foundation
+ *
+ *     The updated version of eGov suite of products as by eGovernments Foundation
+ *     is available at http://www.egovernments.org
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see http://www.gnu.org/licenses/ or
+ *     http://www.gnu.org/licenses/gpl.html .
+ *
+ *     In addition to the terms of the GPL license to be adhered to in using this
+ *     program, the following additional terms are to be complied with:
+ *
+ *         1) All versions of this program, verbatim or modified must carry this
+ *            Legal Notice.
+ *
+ *         2) Any misrepresentation of the origin of the material is prohibited. It
+ *            is required that all modified versions of this material be marked in
+ *            reasonable ways as different from the original version.
+ *
+ *         3) This license does not grant any rights to any user of the program
+ *            with regards to rights under trademark law for use of the trade names
+ *            or trademarks of eGovernments Foundation.
+ *
+ *   In case of any queries, you can reach eGovernments Foundation at contact@egovernments.org.
+ */
+
+package org.egov.ukdcustomservice.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+
+import org.egov.common.contract.request.RequestInfo;
+import org.egov.ukdcustomservice.service.LogService;
+import org.egov.ukdcustomservice.web.models.Pod;
+
+@RestController
+@RequestMapping
+public class LogController {
+
+    @Autowired
+    private LogService logService;
+
+    @SuppressWarnings("rawtypes")
+    @PostMapping("/logs")
+    public ResponseEntity logs(@RequestBody RequestInfo requestInfo, @RequestParam @NotNull String name,
+            @RequestParam String follow, @RequestParam String tail) {
+        InputStreamResource inputStreamResource = logService.getLogs(name, follow, tail);
+
+        if (inputStreamResource != null)
+            return new ResponseEntity<InputStreamResource>(inputStreamResource, HttpStatus.OK);
+        else
+            return new ResponseEntity<String>("Error while getting logs", HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
+
+    @PostMapping("/pods")
+    public List<Pod> pods(@RequestBody RequestInfo requestInfo) {
+        return logService.getPogs();
+    }
+
+}
