d6339e02f0e14e5cea746fb79185b3a0d23d0dd6
priyankarani-eGov <priyanka.rani@egovernments.org>
2020-09-09 12:20:36 +0530

    Update id_seq_gen_script_pt.sql

--- scripts/id_seq_gen_script_pt.sql
+++ scripts/id_seq_gen_script_pt.sql
@@ -5,6 +5,7 @@ DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_roorkee;
 DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_rudrapur;
 DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_kashipur;
 DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_rishikesh;
+DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_chamba;
 
 CREATE SEQUENCE seq_eg_pt_ptid_uk_haldwani;
 CREATE SEQUENCE seq_eg_pt_ptid_uk_haridwar;
@@ -13,3 +14,4 @@ CREATE SEQUENCE seq_eg_pt_ptid_uk_roorkee;
 CREATE SEQUENCE seq_eg_pt_ptid_uk_rudrapur;
 CREATE SEQUENCE seq_eg_pt_ptid_uk_kashipur;
 CREATE SEQUENCE seq_eg_pt_ptid_uk_rishikesh;
+CREATE SEQUENCE seq_eg_pt_ptid_uk_chamba;
