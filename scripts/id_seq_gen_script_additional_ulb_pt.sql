9cf3c0494dd8a183e3e04e4c05f9ab74702ba30a
priyankarani-eGov <priyanka.rani@egovernments.org>
2020-09-09 14:00:24 +0530

    Update id_seq_gen_script_additional_ulb_pt.sql

--- scripts/id_seq_gen_script_additional_ulb_pt.sql
+++ scripts/id_seq_gen_script_additional_ulb_pt.sql
@@ -1,3 +1,5 @@
 DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_chamba;
+DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_badrinath;
 
 CREATE SEQUENCE seq_eg_pt_ptid_uk_chamba;
+CREATE SEQUENCE seq_eg_pt_ptid_uk_badrinath;
