9e0f97b549bdf6ed71a2307e65adf27063b325a8
priyankarani-eGov <priyanka.rani@egovernments.org>
2020-09-29 01:18:49 +0530

    Update id_seq_gen_script_additional_ulb_pt.sql

--- scripts/id_seq_gen_script_additional_ulb_pt.sql
+++ scripts/id_seq_gen_script_additional_ulb_pt.sql
@@ -3,7 +3,8 @@ DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_badrinath;
 DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_swargashramjonk;
 DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_narendranagar;
 DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_dugadda;
-DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_lohaghat
+DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_lohaghat;
+DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_kotdwar
 
 CREATE SEQUENCE seq_eg_pt_ptid_uk_swargashramjonk;
 CREATE SEQUENCE seq_eg_pt_ptid_uk_narendranagar;
@@ -11,3 +12,4 @@ CREATE SEQUENCE seq_eg_pt_ptid_uk_dugadda;
 CREATE SEQUENCE seq_eg_pt_ptid_uk_chamba;
 CREATE SEQUENCE seq_eg_pt_ptid_uk_badrinath;
 CREATE SEQUENCE seq_eg_pt_ptid_uk_lohaghat;
+CREATE SEQUENCE seq_eg_pt_ptid_uk_kotdwar
