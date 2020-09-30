6c56676480588cd9d8cd26470cfea8785c3f9c8e
priyankarani-eGov <priyanka.rani@egovernments.org>
2020-09-30 23:51:03 +0530

    Update id_seq_gen_script_additional_ulb_pt.sql

--- scripts/id_seq_gen_script_additional_ulb_pt.sql
+++ scripts/id_seq_gen_script_additional_ulb_pt.sql
@@ -4,7 +4,14 @@ DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_swargashramjonk;
 DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_narendranagar;
 DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_dugadda;
 DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_lohaghat;
-DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_kotdwar
+DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_kotdwar;
+DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_chamoligopeshwar;
+DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_joshimath;
+DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_manglaur;
+DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_mussorie;
+DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_ramnagar;
+DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_rudraprayag;
+DROP SEQUENCE IF EXISTS seq_eg_pt_ptid_uk_vikasnagar;
 
 CREATE SEQUENCE seq_eg_pt_ptid_uk_swargashramjonk;
 CREATE SEQUENCE seq_eg_pt_ptid_uk_narendranagar;
@@ -12,4 +19,11 @@ CREATE SEQUENCE seq_eg_pt_ptid_uk_dugadda;
 CREATE SEQUENCE seq_eg_pt_ptid_uk_chamba;
 CREATE SEQUENCE seq_eg_pt_ptid_uk_badrinath;
 CREATE SEQUENCE seq_eg_pt_ptid_uk_lohaghat;
-CREATE SEQUENCE seq_eg_pt_ptid_uk_kotdwar
+CREATE SEQUENCE seq_eg_pt_ptid_uk_kotdwar;
+CREATE SEQUENCE seq_eg_pt_ptid_uk_chamoligopeshwar;
+CREATE SEQUENCE seq_eg_pt_ptid_uk_joshimath;
+CREATE SEQUENCE seq_eg_pt_ptid_uk_manglaur;
+CREATE SEQUENCE seq_eg_pt_ptid_uk_mussorie;
+CREATE SEQUENCE seq_eg_pt_ptid_uk_ramnagar;
+CREATE SEQUENCE seq_eg_pt_ptid_uk_rudraprayag;
+CREATE SEQUENCE seq_eg_pt_ptid_uk_vikasnagar;
