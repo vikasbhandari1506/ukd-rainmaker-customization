256c4995ef0cc484d9b8cc6dd1ba22dbb1f300dc
khemanth-eGov <k.hemanth@egovernments.org>
2021-03-26 09:18:24 +0530

    UKI-1100 Configuration changes related to the rollover

--- ukd-custom-service/src/main/resources/db/migration/main/V20210308138999__pt_rolloverbatch_ddl.sql
+++ ukd-custom-service/src/main/resources/db/migration/main/V20210308138999__pt_rolloverbatch_ddl.sql
@@ -2,6 +2,6 @@ CREATE TABLE eg_pt_rolloverbatch (
    id 					CHARACTER VARYING (128) NOT NULL,
    batch 				bigint NOT NULL,
    batchsize 			bigint NOT NULL,
-   createdtime 			bigint NOT NULL,
+   createdtime 			bigint NOT NULL
 
 );
